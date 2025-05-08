395b823402a00ae2ccc68c4bd1986e2370bb1cab
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-05-08 19:18:19 +0200

    Created users by accident

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -2,11 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const { PORT } = require('./util/config')
 const { startDB } = require('./util/db')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users.js')
+
 const { errorHandler, unknownEndpoint } = require('./middleware')
 
 const app = express()
 app.use(express.json())
-app.use('/api/blogs', require('./controllers/blogs'))
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(errorHandler)
 app.use(unknownEndpoint)
 
